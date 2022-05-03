2bd0faf1b2adcc8342707385937654c73800f7ec
YannParis42 <yann.paris.42@gmail.com>
2022-05-03 13:58:36 +0200

    games

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
 {
-    #[Route('/game', name: 'app_game')]
+    public function __construct(private GameRepository $gameRepository) {
+        $this->gameRepository = $gameRepository;
+    }
+
+        #[Route('/game', name: 'app_game')]
     public function index(): Response
     {
+        $games = $this->gameRepository->findAll();
+
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'games' => $games,
         ]);
     }
 }
