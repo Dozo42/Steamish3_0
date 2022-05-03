8bb8a681942518bcc21631bac262181b9cec6f62
YannParis42 <yann.paris.42@gmail.com>
2022-05-03 16:22:48 +0200

    e

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -59,4 +59,10 @@ class GameRepository extends ServiceEntityRepository
         ;
 
     }
+
+    public function getMostBoughtGames(int $limit = 9) {
+
+        return $this->createQueryBuilder('g')
+            ->join(Library::class, )
+    }
 }
