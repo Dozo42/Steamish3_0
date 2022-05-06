4216dcd4119925ca1c50fa468870a374ce3a9cac
YannParis42 <yann.paris.42@gmail.com>
2022-05-06 12:52:25 +0200

    ùform

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\AccountType;
 use App\Repository\AccountRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 #[Route('/utilisateur')]
 class UserController extends AbstractController
@@ -15,14 +20,33 @@ class UserController extends AbstractController
         
     }
 
-    #[Route('/utilisateurs', name: 'app_user_index')]
+    #[Route('/', name: 'app_user_index')]
     public function indexAccount(): Response
     {
         return $this->render('user/index.html.twig', [
-            'account'=>$this->accountRepository->findAll()
+            'account'=>$this->accountRepository->findBy([], ['createdAt' => 'DESC'])
         ]);
     }
     
+    #[Route('/nouveau', name: 'app_new_user')]
+    public function createAccount(Request $request, EntityManagerInterface $em): Response
+    {
+        $form =$this->createForm(AccountType::class, new Account());
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+              /** @var Country $country */
+              $account = $form->getData();
+              $account->setCreatedAt(new Date());
+              $account->setWallet(0);
+              $em->persist($form->getData());
+              $em->flush();
+              return $this->redirectToRoute('app_user_index');
+        }
+        return $this->render('user/new.html.twig',[
+            'form'=>$form->createView(),
+        ]);
+
+    }
 
     #[Route('/{name}', name: 'app_user')]
     public function show(string $name): Response
@@ -31,4 +55,6 @@ class UserController extends AbstractController
             'account' => $this->accountRepository->getAccountAllDetails($name)
         ]);
     }
+
+
 }
